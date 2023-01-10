e2d8c6987863db4735af275d52e2609c38c76b2e
Devin Riley <devin.riley144@gmail.com>
2023-01-10 13:16:57 -0600

    finished excercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public BankAccount()
+        {
+        }
+
+        public BankAccount(double amount)
+        {
+            _balance = amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simulating a deposit of {amount, 0:c} to your account");
+            _balance = amount;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+        public void Withdrawal(double amount)
+        {
+            _balance -= amount;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount myAccount = new BankAccount();
+
+            Console.WriteLine("Enter amount you'd like to deposit");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            myAccount.Deposit(amountToDeposit);
+            double userBalance = myAccount.GetBalance();
+
+            Console.WriteLine($"Your current balance is: {userBalance,0:c}");
+ 
         }
     }
 }
